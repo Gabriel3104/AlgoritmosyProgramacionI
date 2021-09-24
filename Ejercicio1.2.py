dce62aed028af486f531651da358cd6157d8cfd9
Cesar <Gabrielsaleha@gmail.com>
2021-09-24 12:02:21 -0300

    Este es el ejercicio 1.2 desarrollado

---
+++ Ejercicio1.2.py
@@ -0,0 +1,19 @@
+a=int(raw_input("Escribe un numero: "))
+b=int(raw_input("Escribe un numero: "))
+
+def multiplicacion(a,b):
+    return a * b
+
+print (multiplicacion(a,b))
+
+
+    
+    
+    
+
+
+    
+    
+
+
+    
