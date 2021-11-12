80383b06d768cde34011557278762eb84bf00c21
Gabriel Saleha <gabrielsaleha9@gmail.com>
2021-11-12 09:23:42 -0300

    Ejercicio1.6A.py

---
+++ Ejercicio1.6A.py
@@ -0,0 +1,8 @@
+#Escribir una funcion que: Dado dos numeros, imprimir la suma, resta, multiplicacion y division de ambos.
+
+def ImprimirOperacionesMatematicas(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+    
