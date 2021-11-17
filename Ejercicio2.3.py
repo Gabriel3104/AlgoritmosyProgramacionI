96b1fbf38468d53e3dd5ed54a9066d6246873e6f
Gabriel Saleha <gabrielsaleha9@gmail.com>
2021-11-17 10:51:06 -0300

    Ejercicio2.3.py

---
+++ Ejercicio2.3.py
@@ -0,0 +1,6 @@
+"""Escribir una función que convierta un valor dado en grados Fahrenheit a grados
+Celsius. Recordar que la fórmula para la conversión es: 𝐹 = 9 𝐶 + 32
+                                                            5        """
+
+def conversionGradosCelcius(farenheit):
+    print((farenheit-32)/1.8)
